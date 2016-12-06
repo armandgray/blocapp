b5852b715cfdcf7a851d6d0c59a84c8d22d52a7d
Armand Gray <armandgray@gmail.com>
2016-12-06 10:11:33 -0800

    added adapter to rvTopResources and mocked items to lstTopResources

--- app/src/main/java/org/blocorganization/blocapp/bloc/ResourcesSubFragment.java
+++ app/src/main/java/org/blocorganization/blocapp/bloc/ResourcesSubFragment.java
@@ -3,6 +3,8 @@ package org.blocorganization.blocapp.bloc;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +12,14 @@ import android.widget.TextView;
 
 import org.blocorganization.blocapp.R;
 import org.blocorganization.blocapp.models.Resource;
+import org.blocorganization.blocapp.utils.RecyclerItemClickListener;
+import org.blocorganization.blocapp.utils.ResourceItemAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.blocorganization.blocapp.models.RecordType.ACADEMIC;
+import static org.blocorganization.blocapp.models.RecordType.TIPSANDTRICKS;
 
 
 /**
@@ -53,8 +60,27 @@ public class ResourcesSubFragment extends Fragment {
         initializeTvReferencesFrom(rootView);
         collapseAllEmptyViews();
 
+        Resource tip = new Resource.Builder("Use JSTOR")
+                .description("When writing essays, always start by writing out your ideas and then find sources on JSTOR to cite. Not the other way around!")
+                .admin("Armand Gray")
+                .type(TIPSANDTRICKS.toString())
+                .subType(ACADEMIC.toString())
+                .isPublic(true).build();
+        lstTopResources.add(tip);
+        lstTopResources.add(tip);
 
-
+        ResourceItemAdapter adptTopResources = new ResourceItemAdapter(lstTopResources);
+        RecyclerView rvTopResources = (RecyclerView) rootView.findViewById(R.id.rvTopResources);
+        rvTopResources.setAdapter(adptTopResources);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        rvTopResources.setLayoutManager(layoutManager);
+        rvTopResources.addOnItemTouchListener(new RecyclerItemClickListener(getContext(),
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+                    }
+                })
+        );
         return rootView;
     }
 
