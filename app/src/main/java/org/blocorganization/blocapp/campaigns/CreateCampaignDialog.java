c9cd32c032bb7856cf0356969560666900a946f8
Armand Gray <armandgray@gmail.com>
2016-12-08 12:32:02 -0800

    commented out all date related field; refactoring date code into Presenter.class

--- app/src/main/java/org/blocorganization/blocapp/campaigns/CreateCampaignDialog.java
+++ app/src/main/java/org/blocorganization/blocapp/campaigns/CreateCampaignDialog.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,11 +27,9 @@ import org.blocorganization.blocapp.R;
 import org.blocorganization.blocapp.models.Campaign;
 import org.blocorganization.blocapp.utils.ConfirmChangesDialogFragment;
 import org.blocorganization.blocapp.utils.DateTimePickerFragment;
-import org.blocorganization.blocapp.utils.GetDpMeasurement;
 import org.blocorganization.blocapp.utils.ImageThemeAdapter;
 import org.blocorganization.blocapp.utils.RecyclerItemClickListener;
 import org.blocorganization.blocapp.utils.SpinnerAdapter;
-import org.joda.time.DateTime;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,7 +37,6 @@ import java.util.List;
 import static org.blocorganization.blocapp.campaigns.CreateCampaignDialogUtilities.saveCampaignToDatabaseWith;
 import static org.blocorganization.blocapp.campaigns.CreateCampaignDialogUtilities.startDetailActivityWith;
 import static org.blocorganization.blocapp.campaigns.UploadButtonIncluder.setupUploadButtonFrom;
-import static org.blocorganization.blocapp.utils.DateTimeFormatHandler.setTextForDateWith;
 import static org.blocorganization.blocapp.utils.FieldUtilities.AMBITION;
 import static org.blocorganization.blocapp.utils.FieldUtilities.BENEFITS_TO_THE_COLLEGE;
 import static org.blocorganization.blocapp.utils.FieldUtilities.DESCRIPTION;
@@ -73,15 +69,15 @@ public class CreateCampaignDialog extends DialogFragment
     private List<String> venues = new ArrayList<>();
     private List<String> types = new ArrayList<>();
 
-    // Edit Date fields
-    private Boolean isRange;
-    private boolean editEndDate;
-    private RelativeLayout editDateLayout;
-    private RelativeLayout editDateFromLayout;
-    private RelativeLayout editDateEndLayout;
+//    // Edit Date fields
+//    private Boolean isRange;
+//    private boolean editEndDate;
+//    private RelativeLayout editDateLayout;
+//    private RelativeLayout editDateFromLayout;
+//    private RelativeLayout editDateEndLayout;
     private TextView tvToDate;
     private TextView tvFromDate;
-    private ImageView ivToDateMenuArrow;
+//    private ImageView ivToDateMenuArrow;
 
     // upload media fields
     private ImageView ivUpload;
@@ -224,8 +220,8 @@ public class CreateCampaignDialog extends DialogFragment
             }
         });
 
-        assignDateFields(rootView);
-        setupDateClickListeners();
+//        assignDateFields(rootView);
+//        setupDateClickListeners();
 
         setupUploadButtonFrom(rootView, this);
         ivUpload = (ImageView) rootView.findViewById(R.id.ivUpload);
@@ -315,81 +311,81 @@ public class CreateCampaignDialog extends DialogFragment
         return areFieldsNonEmpty;
     }
 
-    private void assignDateFields(View rootView) {
-        editDateLayout = (RelativeLayout) rootView.findViewById(R.id.editDateLayout);
-        editDateFromLayout = (RelativeLayout) rootView.findViewById(R.id.editDateFromLayout);
-        editDateEndLayout = (RelativeLayout) rootView.findViewById(R.id.editDateEndLayout);
-        tvFromDate = (TextView) rootView.findViewById(R.id.tvFromDate);
-        tvToDate = (TextView) rootView.findViewById(R.id.tvToDate);
-        ivToDateMenuArrow = (ImageView) rootView.findViewById(R.id.ivMenuArrow);
-    }
+//    private void assignDateFields(View rootView) {
+//        editDateLayout = (RelativeLayout) rootView.findViewById(R.id.editDateLayout);
+//        editDateFromLayout = (RelativeLayout) rootView.findViewById(R.id.editDateFromLayout);
+//        editDateEndLayout = (RelativeLayout) rootView.findViewById(R.id.editDateEndLayout);
+//        tvFromDate = (TextView) rootView.findViewById(R.id.tvFromDate);
+//        tvToDate = (TextView) rootView.findViewById(R.id.tvToDate);
+//        ivToDateMenuArrow = (ImageView) rootView.findViewById(R.id.ivMenuArrow);
+//    }
 
-    private void setupDateClickListeners() {
-        editDateEndLayout.setVisibility(View.GONE);
-        editDateFromLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new DateTimePickerFragment()
-                        .show(getChildFragmentManager(), DATE_TIME_PICKER);
-            }
-        });
-
-        editDateEndLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new DateTimePickerFragment()
-                        .show(getChildFragmentManager(), DATE_TIME_PICKER);
-                setupDateInitialState();
-            }
-        });
-        editDateFromLayout.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                showEndDateView();
-                return isRange = true;
-            }
-        });
-        editDateEndLayout.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                return hideEndDateView();
-            }
-        });
-    }
-
-    private void setupDateInitialState() {
-        tvToDate.setTextColor(Color.parseColor("#FFFFFF"));
-        ivToDateMenuArrow.setColorFilter(Color.parseColor("#FFFFFF"));
-        ivToDateMenuArrow.setImageResource(R.drawable.ic_menu_down_white_48dp);
-        isRange = true;
-        editEndDate = true;
-    }
-
-    private void loadDateFields() {
-        setTextForDateWith(campaign.getFromDate(), tvFromDate, true);
-        if (campaign.getToDate() != null) {
-            setTextForDateWith(campaign.getToDate(), tvToDate, false);
-            showEndDateView();
-        }
-    }
-
-    private void showEndDateView() {
-        editDateEndLayout.setPadding(GetDpMeasurement.getDPI(getActivity(), 10), GetDpMeasurement.getDPI(getActivity(), 5), 0, 0);
-        editDateEndLayout.setVisibility(View.VISIBLE);
-
-        tvToDate.setTextColor(Color.parseColor("#333333"));
-        ivToDateMenuArrow.setImageResource(R.drawable.ic_menu_down_white_48dp);
-        ivToDateMenuArrow.setColorFilter(Color.parseColor("#333333"));
-
-        editDateFromLayout.setBackgroundResource(R.drawable.date_divider_background);
-        editDateFromLayout.setPadding(GetDpMeasurement.getDPI(getActivity(), 10), 0, 0, GetDpMeasurement.getDPI(getActivity(), 5));
-    }
-
-    private boolean hideEndDateView() {
-        editDateLayout.getChildAt(2).setVisibility(View.GONE);
-        editDateLayout.getChildAt(1).setBackgroundResource(R.color.colorPrimaryDark);
-        return isRange = false;
-    }
+//    private void setupDateClickListeners() {
+//        editDateEndLayout.setVisibility(View.GONE);
+//        editDateFromLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new DateTimePickerFragment()
+//                        .show(getChildFragmentManager(), DATE_TIME_PICKER);
+//            }
+//        });
+//
+//        editDateEndLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new DateTimePickerFragment()
+//                        .show(getChildFragmentManager(), DATE_TIME_PICKER);
+//                setupDateInitialState();
+//            }
+//        });
+//        editDateFromLayout.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                showEndDateView();
+//                return isRange = true;
+//            }
+//        });
+//        editDateEndLayout.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View v) {
+//                return hideEndDateView();
+//            }
+//        });
+//    }
+//
+//    private void setupDateInitialState() {
+//        tvToDate.setTextColor(Color.parseColor("#FFFFFF"));
+//        ivToDateMenuArrow.setColorFilter(Color.parseColor("#FFFFFF"));
+//        ivToDateMenuArrow.setImageResource(R.drawable.ic_menu_down_white_48dp);
+//        isRange = true;
+//        editEndDate = true;
+//    }
+//
+//    private void loadDateFields() {
+//        setTextForDateWith(campaign.getFromDate(), tvFromDate, true);
+//        if (campaign.getToDate() != null) {
+//            setTextForDateWith(campaign.getToDate(), tvToDate, false);
+//            showEndDateView();
+//        }
+//    }
+//
+//    private void showEndDateView() {
+//        editDateEndLayout.setPadding(GetDpMeasurement.getDPI(getActivity(), 10), GetDpMeasurement.getDPI(getActivity(), 5), 0, 0);
+//        editDateEndLayout.setVisibility(View.VISIBLE);
+//
+//        tvToDate.setTextColor(Color.parseColor("#333333"));
+//        ivToDateMenuArrow.setImageResource(R.drawable.ic_menu_down_white_48dp);
+//        ivToDateMenuArrow.setColorFilter(Color.parseColor("#333333"));
+//
+//        editDateFromLayout.setBackgroundResource(R.drawable.date_divider_background);
+//        editDateFromLayout.setPadding(GetDpMeasurement.getDPI(getActivity(), 10), 0, 0, GetDpMeasurement.getDPI(getActivity(), 5));
+//    }
+//
+//    private boolean hideEndDateView() {
+//        editDateLayout.getChildAt(2).setVisibility(View.GONE);
+//        editDateLayout.getChildAt(1).setBackgroundResource(R.color.colorPrimaryDark);
+//        return isRange = false;
+//    }
 
     @Override
     public void onDatePickerCancel() {
@@ -401,44 +397,44 @@ public class CreateCampaignDialog extends DialogFragment
     @Override
     public void onEventDateTimeSet(int year, int month, int day, int hourOfDay, String minute) {
         // Date formatting fields
-        ArrayList<Integer> date = new ArrayList<>();
-        date.add(year);
-        date.add(month);
-        date.add(day);
-        date.add(hourOfDay);
-        date.add(Integer.valueOf(minute));
-
-        DateTime dt = new DateTime();
-        String ampmDesignator = "am";
-        if (hourOfDay >= 12) {
-            ampmDesignator = "pm";
-        }
-        if (hourOfDay != 12) {
-            hourOfDay = hourOfDay % 12;
-        }
-        dt.withDate(year, month, day);
-
-        // initial state is null; afterwards editEndDate is true if user selects 2nd fromDate field
-        if (isRange == null) {
-            editDateFromLayout.setBackgroundResource(R.drawable.date_divider_background);
-            editDateFromLayout.setPadding(GetDpMeasurement.getDPI(getActivity(), 10), 0, 0, GetDpMeasurement.getDPI(getActivity(), 5));
-            tvFromDate.setText("On: " + dt.monthOfYear().getAsText() + " " + day + ", " + year + ", " + hourOfDay + ":" + minute + " " + ampmDesignator);
-            campaign.setFromDate(date);
-            showEndDateView();
-        } else if (editEndDate && isRange) {
-            tvToDate.setText("End: " + dt.monthOfYear().getAsText() + " " + day + ", " + year + ", " + hourOfDay + ":" + minute + " " + ampmDesignator);
-            campaign.setToDate(date);
-            editEndDate = false;
-        } else {
-            tvFromDate.setText("On: " + dt.monthOfYear().getAsText() + " " + day + ", " + year + ", " + hourOfDay + ":" + minute + " " + ampmDesignator);
-        }
+//        ArrayList<Integer> date = new ArrayList<>();
+//        date.add(year);
+//        date.add(month);
+//        date.add(day);
+//        date.add(hourOfDay);
+//        date.add(Integer.valueOf(minute));
+//
+//        DateTime dt = new DateTime();
+//        String ampmDesignator = "am";
+//        if (hourOfDay >= 12) {
+//            ampmDesignator = "pm";
+//        }
+//        if (hourOfDay != 12) {
+//            hourOfDay = hourOfDay % 12;
+//        }
+//        dt.withDate(year, month, day);
+//
+//        // initial state is null; afterwards editEndDate is true if user selects 2nd fromDate field
+//        if (isRange == null) {
+//            editDateFromLayout.setBackgroundResource(R.drawable.date_divider_background);
+//            editDateFromLayout.setPadding(GetDpMeasurement.getDPI(getActivity(), 10), 0, 0, GetDpMeasurement.getDPI(getActivity(), 5));
+//            tvFromDate.setText("On: " + dt.monthOfYear().getAsText() + " " + day + ", " + year + ", " + hourOfDay + ":" + minute + " " + ampmDesignator);
+//            campaign.setFromDate(date);
+//            showEndDateView();
+//        } else if (editEndDate && isRange) {
+//            tvToDate.setText("End: " + dt.monthOfYear().getAsText() + " " + day + ", " + year + ", " + hourOfDay + ":" + minute + " " + ampmDesignator);
+//            campaign.setToDate(date);
+//            editEndDate = false;
+//        } else {
+//            tvFromDate.setText("On: " + dt.monthOfYear().getAsText() + " " + day + ", " + year + ", " + hourOfDay + ":" + minute + " " + ampmDesignator);
+//        }
     }
 
     private void loadCampaignData() {
         if (getArguments() != null) {
             setSelectionForSpinnerFromList(types, campaign.getRecordType(), spType);
             setSelectionForSpinnerFromList(venues, campaign.getVenue(), spVenue);
-            loadDateFields();
+//            loadDateFields();
             setTextForEditTextWith(campaign.getTitle(), etTitle);
             setTextForEditTextWith(campaign.getAbbreviation(), etAbbreviation);
             setTextForEditTextWith(campaign.getAdmin(), etAdmin);
--- app/src/main/java/org/blocorganization/blocapp/utils/DateTimePresenter.java
+++ app/src/main/java/org/blocorganization/blocapp/utils/DateTimePresenter.java
@@ -1,5 +1,29 @@
 package org.blocorganization.blocapp.utils;
 
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
 public class DateTimePresenter {
+
+    private View rootView;
+    private Fragment fragment;
+
+    private Boolean isRange;
+    private boolean editEndDate;
+    private RelativeLayout editDateLayout;
+    private RelativeLayout editDateFromLayout;
+    private RelativeLayout editDateEndLayout;
+    private TextView tvToDate;
+    private TextView tvFromDate;
+    private ImageView ivToDateMenuArrow;
+
+    public DateTimePresenter(View rootView, Fragment fragment) {
+        this.rootView = rootView;
+        this.fragment = fragment;
+    }
+
 
 }
