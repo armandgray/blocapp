45411d0293b83dfb84b00fc60eb1d6cab8fca396
Armand Gray <armandgray@gmail.com>
2017-04-26 16:42:04 -0700

    removed all menu bullets less one from Campaigns frag

--- app/src/main/java/org/blocorganization/blocapp/campaigns/CampaignsFragment.java
+++ app/src/main/java/org/blocorganization/blocapp/campaigns/CampaignsFragment.java
@@ -25,9 +25,6 @@ public class CampaignsFragment extends Fragment
     private ViewPager vpPager;
     private LinearLayout menu;
     private ImageView menuBullet0;
-    private ImageView menuBullet1;
-    private ImageView menuBullet2;
-    private ImageView menuBullet3;
 
     // Swipe Gesture Fields
     private GestureDetector gestureDetector;
@@ -52,56 +49,17 @@ public class CampaignsFragment extends Fragment
         vpPager = (ViewPager) rootView.findViewById(R.id.campaigns_vpager);
         adapterViewPager = new MyPagerAdapter(getChildFragmentManager());
         vpPager.setAdapter(adapterViewPager);
-        vpPager.setCurrentItem(1);
+        vpPager.setCurrentItem(0);
 
         menu = (LinearLayout) rootView.findViewById(R.id.menu_container);
-        menu.removeView(menu.getChildAt(4));
         menuBullet0 = (ImageView) menu.getChildAt(0);
-        menuBullet1 = (ImageView) menu.getChildAt(1);
-        menuBullet2 = (ImageView) menu.getChildAt(2);
-        menuBullet3 = (ImageView) menu.getChildAt(3);
-        menuBullet1.setImageResource(R.drawable.menu_selected_point);
-
-        vpPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                switch (position) {
-                    case 0:
-                        menuBullet1.setImageResource(R.drawable.menu_point);
-                        menuBullet0.setImageResource(R.drawable.menu_selected_point);
-                        return;
-                    case 1:
-                        menuBullet0.setImageResource(R.drawable.menu_point);
-                        menuBullet2.setImageResource(R.drawable.menu_point);
-                        menuBullet1.setImageResource(R.drawable.menu_selected_point);
-                        return;
-                    case 2:
-                        menuBullet1.setImageResource(R.drawable.menu_point);
-                        menuBullet3.setImageResource(R.drawable.menu_point);
-                        menuBullet2.setImageResource(R.drawable.menu_selected_point);
-                        return;
-                    case 3:
-                        menuBullet2.setImageResource(R.drawable.menu_point);
-                        menuBullet3.setImageResource(R.drawable.menu_selected_point);
-                        return;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
+        menuBullet0.setImageResource(R.drawable.menu_selected_point);
 
         return rootView;
     }
 
     public class MyPagerAdapter extends FragmentPagerAdapter {
-        private static final int NUM_PAGES = 4;
+        private static final int NUM_PAGES = 1;
 
         public MyPagerAdapter(FragmentManager fragmentManager) {
             super(fragmentManager);
@@ -119,16 +77,7 @@ public class CampaignsFragment extends Fragment
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return ActionboardSubFragment.newInstance();
-
-                case 1:
-                    return HomeSubFragment.newInstance();
-
-                case 2:
                     return CampaignsSubFragment.newInstance();
-
-                case 3:
-                    return CalendarSubFragment.newInstance();
                 default:
                     return null;
             }
@@ -137,7 +86,7 @@ public class CampaignsFragment extends Fragment
 
     @Override
     public void onCalendarImageClick() {
-        vpPager.setCurrentItem(3);
+        vpPager.setCurrentItem(0);
     }
 
     @Override
