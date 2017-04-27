1691ce91d879adabff30df36be5c99cb12482dd8
Armand Gray <armandgray@gmail.com>
2017-04-26 21:14:19 -0700

    removed lint errors from campaigns pkg

--- app/src/main/java/org/blocorganization/blocapp/campaigns/CampaignsFragment.java
+++ app/src/main/java/org/blocorganization/blocapp/campaigns/CampaignsFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,58 +19,38 @@ import org.blocorganization.blocapp.R;
  */
 public class CampaignsFragment extends Fragment {
 
-    private FragmentPagerAdapter adapterViewPager;
-    private ViewPager vpPager;
-    private LinearLayout menu;
-    private ImageView menuBullet0;
-
-    // Swipe Gesture Fields
-    private GestureDetector gestureDetector;
-
-    private FragmentManager mFragmentManager;
-
-    public CampaignsFragment() {
-        // Required empty public constructor
-    }
+    public CampaignsFragment() {}
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_campaigns, container, false);
 
-        /**
-         *  View Pager - Follow Link for more dynamic options
-         *  https://guides.codepath.com/android/ViewPager-with-FragmentPagerAdapter
-         */
-        vpPager = (ViewPager) rootView.findViewById(R.id.campaigns_vpager);
-        adapterViewPager = new MyPagerAdapter(getChildFragmentManager());
+        ViewPager vpPager = (ViewPager) rootView.findViewById(R.id.campaigns_vpager);
+        FragmentPagerAdapter adapterViewPager = new MyPagerAdapter(getChildFragmentManager());
         vpPager.setAdapter(adapterViewPager);
         vpPager.setCurrentItem(0);
 
-        menu = (LinearLayout) rootView.findViewById(R.id.menu_container);
-        menuBullet0 = (ImageView) menu.getChildAt(0);
+        LinearLayout menu = (LinearLayout) rootView.findViewById(R.id.menu_container);
+        ImageView menuBullet0 = (ImageView) menu.getChildAt(0);
         menuBullet0.setImageResource(R.drawable.menu_selected_point);
 
         return rootView;
     }
 
-    public class MyPagerAdapter extends FragmentPagerAdapter {
+    private class MyPagerAdapter extends FragmentPagerAdapter {
         private static final int NUM_PAGES = 1;
 
-        public MyPagerAdapter(FragmentManager fragmentManager) {
+        MyPagerAdapter(FragmentManager fragmentManager) {
             super(fragmentManager);
-            mFragmentManager = fragmentManager;
         }
 
-        // Returns total number of pages
         @Override
         public int getCount() {
             return NUM_PAGES;
         }
 
-        // Returns the fragment to display for that page
         @Override
         public Fragment getItem(int position) {
             switch (position) {
