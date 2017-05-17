7b0bb80311136f798fd379676ab29be523850e2e
Armand Gray <armandgray@gmail.com>
2017-05-17 13:07:47 -0700

    removed fb instance access from CampaignsSubFrag

--- app/src/main/java/org/blocorganization/blocapp/campaigns/CampaignsSubFragment.java
+++ app/src/main/java/org/blocorganization/blocapp/campaigns/CampaignsSubFragment.java
@@ -20,11 +20,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -36,7 +31,6 @@ import org.blocorganization.blocapp.utils.RecyclerItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -75,97 +69,6 @@ public class CampaignsSubFragment extends Fragment {
 
         mStorage = FirebaseStorage.getInstance().getReference();
         mProgressDialog = new ProgressDialog(getActivity());
-
-        DatabaseReference mCampaignsDatabaseReference = FirebaseDatabase.getInstance().getReference(CAMPAIGNS_CHILD);
-        mCampaignsDatabaseReference.addChildEventListener(new ChildEventListener() {
-
-            @SuppressWarnings("unchecked")
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                Map<String, Object> campaignsSnapshot = (Map) dataSnapshot.getValue();
-                final Campaign campaign = new Campaign();
-
-                campaign.setAbbreviation((String) campaignsSnapshot.get("abbreviation"));
-                campaign.setTitle((String) campaignsSnapshot.get("title"));
-                campaign.setAdmin((String) campaignsSnapshot.get("admin"));
-                campaign.setDescription((String) campaignsSnapshot.get("description"));
-                campaign.setBenefits((String) campaignsSnapshot.get("benefits"));
-                campaign.setAmbition((String) campaignsSnapshot.get("ambition"));
-                campaign.setPlanOfExecution((String) campaignsSnapshot.get("planOfExecution"));
-                campaign.setItemizedBudget((String) campaignsSnapshot.get("itemizedBudget"));
-                campaign.setVenue((String) campaignsSnapshot.get("venue"));
-                campaign.setRecordType((String) campaignsSnapshot.get("recordType"));
-                campaign.setExtras((String) campaignsSnapshot.get("extras"));
-                campaign.setPhotoUrl((String) campaignsSnapshot.get("photoUrl"));
-                campaign.setThemeImageUrl((String) campaignsSnapshot.get("themeImageUrl"));
-                if (campaignsSnapshot.get("isPublic") == null) {
-                    campaign.setPublic(false);
-                } else {
-                    campaign.setPublic((Boolean) campaignsSnapshot.get("isPublic"));
-                }
-
-                ArrayList<Long> timestampAsListLong = (ArrayList<Long>) campaignsSnapshot.get("timestamp");
-                if (timestampAsListLong != null) {
-                    ArrayList<Integer> timestampList = new ArrayList<>();
-                    for (Long dateElement : timestampAsListLong) {
-                        Integer elementAsInteger = dateElement != null ? dateElement.intValue() : null;
-                        timestampList.add(elementAsInteger);
-                    }
-                    campaign.setTimestamp(timestampList);
-                } else {
-                    campaign.setTimestamp(null);
-                }
-
-                ArrayList<Long> fromDateAsListLong = (ArrayList<Long>) campaignsSnapshot.get("fromDate");
-                if (fromDateAsListLong != null) {
-                    ArrayList<Integer> fromDateList = new ArrayList<>();
-                    for (Long dateElement : fromDateAsListLong) {
-                        Integer elementAsInteger = dateElement != null ? dateElement.intValue() : null;
-                        fromDateList.add(elementAsInteger);
-                    }
-                    campaign.setFromDate(fromDateList);
-                } else {
-                    campaign.setFromDate(null);
-                }
-
-                ArrayList<Long> toDateAsListLong = (ArrayList<Long>) campaignsSnapshot.get("toDateList");
-                if (toDateAsListLong != null) {
-                    ArrayList<Integer> toDateList = new ArrayList<>();
-                    for (Long dateElement : toDateAsListLong) {
-                        Integer elementAsInteger = dateElement != null ? dateElement.intValue() : null;
-                        toDateList.add(elementAsInteger);
-                    }
-                    campaign.setToDate(toDateList);
-                } else {
-                    campaign.setToDate(null);
-                }
-
-                campaigns.add(0, campaign);
-                adapter.notifyItemInserted(0);
-
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                // copy code above and use hashcodes to identify the corresponding
-                // position of the campaign changed with List<Campaign> campaigns
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-//                HashMap<String, String> campaign = dataSnapshot.getValue(HashMap.class);
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
 
         final RecyclerView rvCampaigns = (RecyclerView) rootView.findViewById(R.id.rvCampaigns);
         adapter = new CampaignsItemAdapter(getActivity(), true, campaigns);
